637ea1752791bb8d15de89251646d21d5886834e
ddt2014 <dingxu1989@gmail.com>
2017-03-31 09:56:06 +0800

    push

--- command.sh
+++ command.sh
@@ -1,1 +1,10 @@
+#! /bin/sh
+
 raspistill -t 1000 -o ~/cam/src/$(date "+%m%d_%H%M").jpg 
+cd ~/cam
+git add *
+echo "git111 add" > ~/log
+git commit -m 'push'
+echo "git add"
+git push origin master
+echo "git add"
